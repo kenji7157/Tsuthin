005461ceb3a37fbf62fa362c493c7b7361ad5efe
Kawakeee <kawake.kk@gmail.com>
2018-12-21 20:42:43 +0900

    [20181221]前回作業のコミット忘れ

--- app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
+++ app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kawanobe_kenji.tsuthin
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 import okhttp3.OkHttpClient
@@ -11,14 +12,23 @@ import org.jsoup.Jsoup
 import java.io.IOException
 
 
+
+
 class MainActivity : AppCompatActivity() {
 
+    // onCreate アクティビティが起動されたときに呼び出されるメソッド
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // setContentView(ビュー) 画面に表示するビューを設定する関数
         setContentView(R.layout.activity_main)
-
-        // 非同期処理が実行される
-        MyAsyncTask().execute()
+        //
+        findViewById<View>(R.id.getPriceButton).setOnClickListener {
+            // 非同期処理が実行される
+            MyAsyncTask().execute()
+//            val text = editText.getText().toString()
+//            // 取得したテキストを TextView に張り付ける
+//            textView.setText(text)
+        }
     }
 
     // AsyncTaskクラスを継承したMyAsyncTaskをMainActivityクラス内に記述
@@ -33,11 +43,12 @@ class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             // レイアウト上のテキストビュー:mytextオブジェクトを変数textviewに格納
-            val texview = findViewById<TextView>(R.id.mytext)
+            val texview = findViewById<TextView>(R.id.priceText)
             //
             texview.setText(result)
         }
     }
+
 }
 
 fun getHtml(): String {
@@ -61,6 +72,5 @@ fun getHtml(): String {
         e.printStackTrace()
     }
     return ""
-    //return resp.body()!!.string()
 }
 
