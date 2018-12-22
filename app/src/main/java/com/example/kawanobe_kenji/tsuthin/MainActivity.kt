a19938aa0744081c5678421087fca01b7dd19b2e
Kawakeee <kawake.kk@gmail.com>
2018-12-22 16:36:33 +0900

    [20181222]メイン画面とレコード画面の実装、レコード画面のListViewの実装

--- app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
+++ app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.kawanobe_kenji.tsuthin
 
+import android.app.Activity
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.View
 import android.widget.TextView
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.ListView
 
 import org.jsoup.Jsoup
 import java.io.IOException
+import android.R.attr.data
+import android.widget.ArrayAdapter
 
 
 
-
+// タイトルバーを外しておくためにはMainActivityの継承はActivityとする
 class MainActivity : AppCompatActivity() {
 
     // onCreate アクティビティが起動されたときに呼び出されるメソッド
@@ -19,12 +26,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // setContentView(ビュー) 画面に表示するビューを設定する関数
         setContentView(R.layout.activity_main)
+        // Toolbar用の変数を用意
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // setSupportActionBar()によってtoolbarをActionBarと同じ様に使う事ができる
+        setSupportActionBar(toolbar)
 
         // getPriceButton押下時のイベント処理
         findViewById<View>(R.id.getPriceButton).setOnClickListener {
             // 非同期処理が実行される
             MyAsyncTask().execute()
         }
+    }
+
+    // ツールバーに表示される項目を追加する
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    // 設置したメニューの項目がクリックされた時に呼ばれる
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.home -> {
+                // main画面に切り替える
+                setContentView(R.layout.activity_main)
+                // Toolbar用の変数を用意
+                var toolbar = findViewById<Toolbar>(R.id.toolbar)
+                // setSupportActionBar()によってtoolbarをActionBarと同じ様に使う事ができる
+                setSupportActionBar(toolbar)
+            }
+
+            R.id.record -> {
+                // record画面に切り替える
+                setContentView(R.layout.activity_record)
+                // Toolbar用の変数を用意
+                var toolbar = findViewById<Toolbar>(R.id.toolbar)
+                // setSupportActionBar()によってtoolbarをActionBarと同じ様に使う事ができる
+                setSupportActionBar(toolbar)
+
+                val listView = findViewById<ListView>(R.id.listView)
+                val items = Array(20, { i -> "Title-$i" })
+                val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+                listView.adapter = adapter
+
+            }
+        }
+        return false
     }
 
     // AsyncTaskクラスを継承したMyAsyncTaskをMainActivityクラス内に記述
@@ -48,25 +95,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-}
+    // 入力されたURLがinput引数となり、引数のURLの商品の価格を返す
+    private fun getPrice(url: String): String {
+        /* スクレイピングの実装 */
+        try {
+            val document = Jsoup.connect(url).get()
+            var elements = document.select("span")
+            for (element in elements) {
+                System.out.println()
 
-// 入力されたURLがinput引数となり、引数のURLの商品の価格を返す
-fun getPrice(url: String): String {
-    /* スクレイピングの実装 */
-    try {
-        val document = Jsoup.connect(url).get()
-        var elements = document.select("span")
-        for (element in elements) {
-            System.out.println()
-
-            if ( element.attr("class").equals("item-price bold") ) {
-                return element.text().toString()
+                if ( element.attr("class").equals("item-price bold") ) {
+                    return element.text().toString()
+                }
             }
+            return elements.text().toString()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-        return elements.text().toString()
-    } catch (e: IOException) {
-        e.printStackTrace()
+        return ""
     }
-    return ""
+
 }
+
+
 
