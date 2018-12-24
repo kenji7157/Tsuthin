1dc2e1594d67fecc4891c64f852ed18cde10f357
Kawakeee <kawake.kk@gmail.com>
2018-12-24 15:42:09 +0900

    [20181224]ソースコードを整理する前にコミット

--- app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
+++ app/src/main/java/com/example/kawanobe_kenji/tsuthin/MainActivity.kt
@@ -1,31 +1,23 @@
 package com.example.kawanobe_kenji.tsuthin
 
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.View
 import android.widget.TextView
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
-import android.widget.ListView
+import com.google.gson.Gson
 
-import org.jsoup.Jsoup
-import java.io.IOException
-import android.widget.ArrayAdapter
-
-
-
-// タイトルバーを外しておくためにはMainActivityの継承はActivityとする
 class MainActivity : AppCompatActivity() {
-
-    // 初期のリスト項目を設定
-    //private var arrayAdapter: MyArrayAdapter? = null
 
     // RecordActivityに送るようのリスト
     private var goodsDataList: ArrayList<GoodsData> = ArrayList<GoodsData>()
 
+    // url情報
+    private var url: String = String()
 
     // onCreate アクティビティが起動されたときに呼び出されるメソッド
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,15 +29,16 @@ class MainActivity : AppCompatActivity() {
         // setSupportActionBar()によってtoolbarをActionBarと同じ様に使う事ができる
         setSupportActionBar(toolbar)
 
-        // adapter初期化
-        //arrayAdapter = MyArrayAdapter(this, 0)
-
+        // goodsDataListへローカル保存されている値を設定する
+        settingList()
 
         // getPriceButton押下時のイベント処理
         findViewById<View>(R.id.getPriceButton).setOnClickListener {
             // 非同期処理が実行される
             System.out.println("非同期処理が実行される")
-            MyAsyncTask().execute()
+            //MyAsyncTask().execute()
+            url = findViewById<TextView>(R.id.urlText).text.toString()
+            GoodsAsyncTask().execute(this)
         }
     }
 
@@ -68,6 +61,8 @@ class MainActivity : AppCompatActivity() {
 //            }
 
             R.id.record -> {
+
+
                 // record画面に切り替える
                 val intent = Intent(this, RecordActivity::class.java)
                 // 変数sendGoodsDataListにsendGoodsDataListを格納する
@@ -95,73 +90,41 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    // AsyncTaskクラスを継承したMyAsyncTaskをMainActivityクラス内に記述
-    inner class MyAsyncTask: AsyncTask<Void, Void, GoodsData>() {
-
-        // doInBackground()は非同期で行いたい処理の内容 返戻値はonPostExecute()の引数となる
-        override fun doInBackground(vararg p0: Void?): GoodsData? {
-            // レイアウト上のテキストビュー:urlTextオブジェクトを変数urTextに格納
-            val urlText = findViewById<TextView>(R.id.urlText)
-            //　返戻値はonPostExecute()の引数となる
-            return getGoods(urlText.text.toString())
-        }
-
-        // onPostExecute()はdoIMnBackground()終了後にメインスレッドで実行される処理の内容
-        override fun onPostExecute(result: GoodsData) {
-            super.onPostExecute(result)
-            // レイアウト上のテキストビュー:mytextオブジェクトを変数textviewに格納
-            val texview = findViewById<TextView>(R.id.priceText)
-            //
-            texview.setText(result.beforePrice)
-            //System.out.println(result?.substring(2))
-            //arrayAdapter?.add(GoodsData(result.name, result.beforePrice,"¥ zzz"))
-            // goodsDataListへ要素を追加
-            goodsDataList.add(GoodsData(result.name, result.beforePrice,"¥ zzz"))
-        }
+    fun addGoodsDataList(goodsData: GoodsData) {
+        this.goodsDataList.add(goodsData)
     }
 
-    // 入力されたURLがinput引数となり、引数のURLの商品の価格を返す
-    private fun getGoods(url: String): GoodsData {
-        /* スクレイピングの実装 */
-        var result :GoodsData = GoodsData()
-        try {
-            // 指定したURLにGETメソッドでアクセス
-            // 結果をパース(テキストのみを取得)し Documentオブジェクトに格納
-            val document = Jsoup.connect(url).get()
-
-            // "h1"タグが持つテキスト群に絞る
-            var h1List = document.select("h1")
-            // "h1"タグが持つテキスト群の要素から商品名を取得する
-            for (element in h1List) {
-                // 商品名の取得
-                // "span"タグの"class"属性値が"item-name"のテキスト要素かの判定
-                if ( element.attr("class").equals("item-name") ) {
-                    // 商品名の設定
-                    result.name = element.text().toString()
-                }
-            }
-
-            // "span"タグが持つテキスト群に絞る
-            var spanList = document.select("span")
-            // "span"タグが持つテキスト群の要素から(登録)価格を取得する
-            for (element in spanList) {
-                // (登録)価格の取得
-                // "span"タグの"class"属性値が"item-price bold"のテキスト要素かの判定
-                if ( element.attr("class").equals("item-price bold") ) {
-                    // 登録価格の設定
-                    result.beforePrice = element.text().toString()
-                }
-            }
-
-            // 商品名の設定
-            result.nowPrice = "¥ ---"
-
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return result
+    fun getGoodsDataList() : ArrayList<GoodsData>{
+        return this.goodsDataList
     }
 
+    // goodsDataListへローカル保存されている値を設定する
+    private fun settingList(){
+        // getDefaultSharedPreferencesで作成したSharedPreferencesのモードはデフォルトでMODE_PRIVATEになる
+        // 以下、MODE_PRIVATEの説明
+        // getSharedPreferences を呼んだ(= ファイルを作成した) アプリケーションからのみアクセスできる
+        // （ただし、同じ user ID を共有しているアプリケーションもアクセスできる）
+        val prefs2 = PreferenceManager.getDefaultSharedPreferences(this)
+        // 保存されている商品情報の数だけ繰り返す
+        for (i in 0..prefs2.all.size) {
+            // getString()にて
+            // 第一引数：保存データのキー値　element0,element1,...elementN
+            // 第二引数：取得できなかった場合の仮値
+            val json = prefs2.getString("element"+i.toString(), "")
+            val gson2 = Gson()
+            // JSONからJavaオブジェクト(GoodsData)への変換
+            val goodsData = gson2.fromJson<GoodsData>(json, GoodsData::class.java!!)
+            if (goodsData != null) {
+                // goodsDataListへ要素を追加する
+                goodsDataList.add(goodsData)
+            }
+        }
+
+    }
+
+    fun getUrl(): String {
+        return this.url
+    }
 }
 
 
